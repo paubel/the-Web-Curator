4a3672064f0b4f97dff8ea78c9654288a6d84b20
paubel <paul.belfrage@gmail.com>
2021-05-24 21:14:40 +0200

    create html, css and javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const pColorchange = document.querySelector('.p-colorchange');
+const btnColorchange = document.querySelector('.btn-colorchange');
+
+btnColorchange.addEventListener('click', function() {
+  pColorchange.style.color = 'red';
+});
